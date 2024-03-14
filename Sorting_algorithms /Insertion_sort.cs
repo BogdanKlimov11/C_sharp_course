bc4ec25118ec91f6a89ec7cba4d814a0f200780a
Bogdan Klimov <136115919+BogdanKlimov11@users.noreply.github.com>
2024-03-14 17:54:54 +0300

    Create Insertion_sort.cs

---
+++ Sorting_algorithms /Insertion_sort.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Insertion_sort {
+    static void insertion_sort(int[] arr) {
+        int n = arr.Length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+}
